3915c30e13a3cb3e2b783df5cc8380a31ffc1ac6
Nicholas <nicholasfp@hotmail.com>
2023-05-13 01:27:34 -0300

    primeiro commit - aula 01

---
+++ aula01.py
@@ -0,0 +1,13 @@
+""" aula 01 """
+print('olá mundo!!!')
+
+
+def somar(numero_1, numero_2):
+    """ soma dois numeros """
+    return numero_1 + numero_2
+
+
+SOMA = somar(10.3, 2.5)
+
+
+print(SOMA)
